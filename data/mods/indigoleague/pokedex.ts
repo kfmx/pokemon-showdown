0cf7381191124ffbc486000c711de9a08cca604f
kfmx <58790668+kfmx@users.noreply.github.com>
2021-06-24 17:26:47 +0200

    Added learnset + pokedex entry for Aerodactyl.

--- data/mods/indigoleague/pokedex.ts
+++ data/mods/indigoleague/pokedex.ts
@@ -4,6 +4,11 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		baseStats: {hp: 80, atk: 75, def: 70, spa: 110, spd: 70, spe: 95},
 		abilities: {0: "Pressure", 1: "Justified", H: "Disaster Warning"}
 	},
+	aerodactyl: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 100, def: 90, spa: 60, spd: 70, spe: 110},
+		abilities: {0: "Unnerve", 1: "Hustle", H: "Imposing"}
+	},
 	feraligatr: {
 		inherit: true,
 		baseStats: {hp: 75, atk: 105, def: 95, spa: 70, spd: 80, spe: 70},
